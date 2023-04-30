40b62a089679dd25496722949f8a8f7d85034eb1
haval95 <havalrezan22@gmail.com>
2023-04-30 22:30:04 +0300

    adding post to the admin so authors can write to the website thro the admin page

--- blog/models.py
+++ blog/models.py
@@ -11,4 +11,8 @@ class Post(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    status = models.IntegerField(choices=STATUS, default=0)
+    status = models.IntegerField(choices=STATUS, default=0) 
+    
+    def __str__(self):
+        return self.title
+        
