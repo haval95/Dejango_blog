5933b8d628829f21aa32a235a2e5c1a8be1eb6ec
haval95 <havalrezan22@gmail.com>
2023-05-01 00:08:57 +0300

    we added about page using templateView

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from . import views
 from django.urls import path
 
-urlpatterns = [path("<slug:slug>", views.BlogView.as_view(), name="blog_view")]
+urlpatterns = [path("<slug:slug>", views.BlogView.as_view(), name="blog_view"), 
+               path("about/", views.AboutViwe.as_view(), name="about_view")]
+
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,7 @@ from django.views import generic
 class BlogView(generic.DetailView):
     model = Post
     template_name = "blog.html"
+
+
+class AboutViwe (generic.TemplateView):
+    template_name = "about.html"
