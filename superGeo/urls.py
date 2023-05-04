db21a06cf1d2c7dac14200cc6687581f2fc65910
haval95 <havalrezan22@gmail.com>
2023-05-05 02:44:54 +0300

    based on the address we add long and latitude, and we also enable the user to download the file if they want

--- superGeo/urls.py
+++ superGeo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.SuperGeo_view, name="supergeo_view")
+    path("", views.SuperGeo_view, name="supergeo_view"),
+    path("download/", views.download, name="download")
 ]
--- superGeo/views.py
+++ superGeo/views.py
@@ -1,15 +1,41 @@
 from django.shortcuts import render
 import pandas as pd
+from geopy.geocoders import ArcGIS
+import csv
+from django.http import HttpResponse
 
+arg = ArcGIS()
+data = None
 
 def SuperGeo_view(request):
-    print(request.POST)
+    global data
     if request.method == 'POST':
         # file = request.POST["file"]
         file = request.FILES['my_file']
         df = pd.read_csv(file)
-        table = df.to_dict('records')
-        print(df)
+        df["cordinates"]= df["address"].apply(arg.geocode)
+        df["Latitude"]=df["cordinates"].apply(lambda cordinate: cordinate.latitude)
+        df["Longitude"]=df["cordinates"].apply(lambda cordinate: cordinate.longitude)
+            
+        table = df.to_dict('records')  
+        data = df
         return render(request, "superGeo.html", {"table": table})
     else:
         return render(request, "superGeo.html")
+    
+    
+def download(request):
+    global data
+    if data is None:
+        # if data is not set, return an error response
+        return HttpResponse('No data to download')
+    else:
+        # create a response object with the CSV file
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="my_objects.csv"'
+        response['Content-Type'] = 'text/csv'
+
+        # write the DataFrame to the response as a CSV file
+        data.to_csv(path_or_buf=response, index=False, quoting=csv.QUOTE_NONNUMERIC)
+        
+        return response
